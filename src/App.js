d08db57f342ddb6b4207db2ffa48ed2fc6fbf82f
Lean Rasmussen <leanmkrasmussen@gmail.com>
2017-02-23 14:39:42 +0100

    added fetch method for import of gifs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+    gifs:[]
+    } 
+
+  }
+  componentDidMount(){
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    .then(responese => responese.json())
+    .then(responseData => {
+      this.setState({gifs :responseData.data})
+    }).catch(error =>{
+      console.log('error in Fetching or parsing data', error)
+    } );
+
+  }
+
   render() {
+    console.log(this.state.gifs)
     return (
       <div className="App">
-      Hello
+        <div className='headerContainer'>
+          <h1>What are you giffing for start giffing already!!</h1>
+          <input type='text'></input> <button>Search</button>
+        </div>
+        <div className='gifContainer'>
+        </div>
+
       </div>
     );
   }
